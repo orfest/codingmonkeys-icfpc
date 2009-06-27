00f8f4d2ca526e46b9614820bcf90b35c975ca44
vizovitin <vizovitin@6cd8edf8-6250-11de-81d0-cf1ed2c911a4>
2009-06-27 11:58:23 +0000

    Removal of excess path points in visualizer. (Not uniform yet)

--- trunk/icfpc_gui/ship.cpp
+++ trunk/icfpc_gui/ship.cpp
@@ -2,4 +2,17 @@
 
 void Ship::pushPosition(const QPointF& p){
     track.push_back(p);
+	removeExcessTrackPoints();
+}
+
+void Ship::removeExcessTrackPoints(int maxPoints) {
+	if (track.size() > maxPoints) {
+		QVector<QPointF> newTrack;
+		newTrack.reserve(track.size());
+		for (int i = 0; i < track.size(); i++) {
+			if (!(i & 1))
+				newTrack.append(track[i]);
+		}
+		track = newTrack;
+	}
 }
