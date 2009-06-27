9e6e7fc89f164b2cd8d558a9d41c7f052c4ce15d
nkurtov <nkurtov@6cd8edf8-6250-11de-81d0-cf1ed2c911a4>
2009-06-27 13:44:04 +0000

    script for running 100x scenarios

---
+++ trunk/testing/100.bat
@@ -0,0 +1,4 @@
+icfpc_gui.exe -p 0 -s 1001 -l log.txt -o res1001.osf
+icfpc_gui.exe -p 0 -s 1002 -l log.txt -o res1002.osf
+icfpc_gui.exe -p 0 -s 1003 -l log.txt -o res1003.osf
+icfpc_gui.exe -p 0 -s 1004 -l log.txt -o res1004.osf
