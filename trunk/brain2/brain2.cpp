863b44b497d73a21ce343b18d13e84fbd10c826d
nkurtov <nkurtov@6cd8edf8-6250-11de-81d0-cf1ed2c911a4>
2009-06-27 15:39:44 +0000

    tracer fixed scenario number

--- trunk/brain2/brain2.cpp
+++ trunk/brain2/brain2.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-B2::B2(int sn):Brain(sn) {}
+B2::B2(int sn):Brain(sn),startTime(-1) {}
 
 PortMapping B2::step(const PortMapping& output){
 	/** /
@@ -28,7 +28,65 @@ PortMapping B2::step(const PortMapping& output){
     if (timestep == 0){
         assert(output.empty());
         res[SCENARIO_PORT] = Brain::scenarioNumber;
+    } else if (timestep == 1) {
+		me_r = sqrt(pow(output.find(EARTH_X)->second, 2) + pow(output.find(EARTH_Y)->second, 2));
+		that_r = output.find(TARGET_RADIUS)->second;
+		transferTime = M_PI * sqrt(pow(me_r + that_r, 3) / (8 * MU_CONST));
+	} else if (timestep == 2) {
+       	delta_v1 = sqrt(MU_CONST / me_r) * (sqrt(2 * that_r / (me_r + that_r)) - 1);
+        
+            Vector prevEarth(prevInput.find(EARTH_X)->second, prevInput.find(EARTH_Y)->second);
+		    Vector curEarth(output.find(EARTH_X)->second, output.find(EARTH_Y)->second);
+		    Vector moveDir = prevEarth - curEarth;
+		    Vector tangent(curEarth.y, -curEarth.x);
+		    tangent.normalize();
+		    me_clockwise = ( Vector::dotProduct(moveDir, tangent) > 0.0 );
+            if (!me_clockwise) {
+			    tangent = -tangent;
+            }
+        
+		    Vector prevTarget(prevInput.find(TARGET_X)->second, prevInput.find(TARGET_Y)->second);
+		    Vector curTarget(output.find(TARGET_X)->second, output.find(TARGET_Y)->second);
+		    Vector targetMoveDir = prevTarget - curTarget;
+		    Vector targetTangent(curTarget.y, -curTarget.x);
+		    targetTangent.normalize();
+		    that_clockwise = ( Vector::dotProduct(targetMoveDir, targetTangent) > 0.0 );
+            if (!that_clockwise) {
+			    targetTangent = -targetTangent;
+            }
+        
+        needTurnAround = (me_clockwise != that_clockwise);
     }
+
+    if (timestep == startTime){
+        Vector curEarth(output.find(EARTH_X)->second, output.find(EARTH_Y)->second);
+	    Vector tangent(curEarth.y, -curEarth.x);
+	    tangent.normalize();
+        if (!me_clockwise) {
+		    tangent = -tangent;
+        }
+        Vector delta = tangent * delta_v1;
+		res[VX_PORT] = delta.x;
+		res[VY_PORT] = delta.y;
+	}
+
+	if (timestep == ceil(startTime + transferTime)) {
+		assert(ceil(startTime + transferTime) > 1);
+
+		double delta_v2 = sqrt(MU_CONST / that_r) * (1 - sqrt(2 * me_r / (me_r + that_r)));
+		Vector curEarth(output.find(EARTH_X)->second, output.find(EARTH_Y)->second);
+		Vector tangent(curEarth.y, -curEarth.x);
+		tangent.normalize();
+        if (!me_clockwise){
+			tangent = -tangent;
+        }
+
+		Vector delta = tangent * delta_v2;
+		res[VX_PORT] = delta.x;
+		res[VY_PORT] = delta.y;
+	}
+
+
     prevResult = res;
     prevInput = output;
 	timestep++;
