69838ae3e002f14dcb930625d488f6a586d7a2c8
gorodilovm <gorodilovm@6cd8edf8-6250-11de-81d0-cf1ed2c911a4>
2009-06-28 17:24:39 +0000

    Brain3_3. Hack!!! But it works for 3001!!!

--- brain3_3/brain3_3.cpp
+++ brain3_3/brain3_3.cpp
@@ -39,14 +39,45 @@ PortMapping B3_3::_step(const PortMapping& output){
 		static FreeFlyToOpositPoint op1;
 		op1.SetTarget(target);
 		operation_list.push(static_cast<Operation*>(&op1));
-		static Hohman el1;
+		static Hohman hoh1;
 		target.minR = orbits[0].minR;
 		target.maxR = orbits[1].minR;
+		hoh1.SetTarget(target);
+		operation_list.push(static_cast<Operation*>(&hoh1));
+		double alp = 7291.5;//estimateTimeToPerihelion(target);
+		double r0 = sqrt(pow(target.maxR.x,2) + pow(target.maxR.y,2));
+		double r1 = sqrt(pow(orbits[1].maxR.x,2) + pow(orbits[1].maxR.y,2));
+		double rt = pow( MU_CONST*pow(alp/(2.0*M_PI),2.0) ,1.0/3) - r0;
+		if (rt < EARTH_RADIUS + 10000){
+			int i = 1;
+			while (rt < EARTH_RADIUS + 10000){
+				rt = pow( MU_CONST*( pow(
+					alp/(2.0*M_PI) + 2.0*i*M_PI*sqrt( pow( (r0+r1)/2.0,3.0 )/MU_CONST ),2.0 ) ) ,1.0/3) - r0;
+				i++;
+			}
+		}
+		target.minR.x = r0;
+		target.minR.y = 0;
+		target.maxR.x = rt;
+		target.maxR.y = 0;
+		static CircleToElliptic el1;
 		el1.SetTarget(target);
 		operation_list.push(static_cast<Operation*>(&el1));
+		static FreeFly wait;
+		wait.transferTime = el1.GetTransferTime();
+		operation_list.push(static_cast<Operation*>(&wait));
+		static EllipticToCircle el2;
+		el2.SetTarget(target);
+		operation_list.push(static_cast<Operation*>(&el2));
+		static CircleToElliptic el3;
+		target.minR.x = r0;
+		target.minR.y = 0;
+		target.maxR.x = r1;
+		target.maxR.y = 0;
+		el3.SetTarget(target);
+		operation_list.push(static_cast<Operation*>(&el3));
     } else {
-        int alp = estimateTimeToPerihelion(target.maxR);
-        int i = 1;
+        
 	}
 
     prevResult = res;
