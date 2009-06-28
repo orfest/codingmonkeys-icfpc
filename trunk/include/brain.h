7029c5789e7160b19343aa27b4bda2764626db7c
gorodilovm <gorodilovm@6cd8edf8-6250-11de-81d0-cf1ed2c911a4>
2009-06-28 10:17:01 +0000

    Add new Brain header.

--- trunk/include/brain.h
+++ trunk/include/brain.h
@@ -2,24 +2,33 @@
 #define BRAIN_H
 
 #include <vector>
-
+#include <queue>
+#include "vector.h"
+#include "operations.h"
 #include "common.h"
 
 class Brain{
 public:
     static Brain* getBrain(int problem, int scenarioNumber);
-    virtual PortMapping step(const PortMapping& output) = 0;
+    PortMapping step(const PortMapping& output);
+	virtual PortMapping _step(const PortMapping& output) = 0;
     bool finished(const PortMapping& output) const;
 
     virtual std::vector<pointF> getShipsPositions() const = 0;
     virtual int getShipsNumber() const = 0;
 
+	static PortMapping prevResult;
+	static PortMapping prevInput;	
 protected:
     Brain(int sn):scenarioNumber(sn),timestep(0){}
     int scenarioNumber;
     int timestep;
+	
+	
+	std::queue<Operation*> operation_list;
+	double time_shift; 
+	STATE state;
 
 	PortMapping & fuelOveruseFailsafe(const PortMapping & sensors, PortMapping & actuators);
 };
-
 #endif //BRAIN_H
