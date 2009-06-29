76c1b9ee1d122a20ba722138749526ac3a796a38
gorodilovm <gorodilovm@6cd8edf8-6250-11de-81d0-cf1ed2c911a4>
2009-06-29 16:41:56 +0000

    Brain3_3 last update

--- icfpc_gui/operations.cpp
+++ icfpc_gui/operations.cpp
@@ -94,6 +94,39 @@ PortMapping CircleToElliptic::step(const PortMapping& output){
 	return res;
 }
 
+PortMapping CircleToEllipticFast::step(const PortMapping& output){
+	PortMapping res;
+	res[SCENARIO_PORT] = 0;
+	res[VX_PORT] = 0;
+	res[VY_PORT] = 0;
+
+	if (timestep == 0) {
+		state = RUNNING;
+		r1 = sqrt(pow(target.minR.x, 2) + pow(target.minR.y, 2));
+		r2 = sqrt(pow(target.maxR.x, 2) + pow(target.maxR.y, 2));
+
+		transferTime = M_PI * sqrt(pow(r1 + r2, 3) / (8 * MU_CONST));
+		double delta_v1 = sqrt(MU_CONST / r1) * (sqrt(2 * r2 / (r1 + r2)) - 1);
+		Vector prevEarth(Brain::prevInput.find(EARTH_X)->second, Brain::prevInput.find(EARTH_Y)->second);
+		Vector curEarth(output.find(EARTH_X)->second, output.find(EARTH_Y)->second);
+		Vector moveDir = prevEarth - curEarth;
+		Vector tangent(curEarth.y, -curEarth.x);
+		tangent.normalize();
+		clockwise = ( Vector::dotProduct(moveDir, tangent) > 0.0 );
+		if (!clockwise)
+			tangent = -tangent;
+
+		Vector delta = tangent * delta_v1;
+		res[VX_PORT] = delta.x;
+		res[VY_PORT] = delta.y;
+		state = COMPLETE;
+	}
+
+
+	timestep++;
+	return res;
+}
+
 
 double CircleToElliptic::GetTransferTime()
 {
